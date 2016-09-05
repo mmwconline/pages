fab0dfb30760389c437c5f0233393a5773cde02d
azizj1 <aazizmj1@gmail.com>
2016-09-04 19:27:48 -0500

    removed magnific script from scripts.js to page.js

--- _src/assets/js/pages/index.js
+++ _src/assets/js/pages/index.js
@@ -31,11 +31,12 @@
 
         // Set Big Image
         if(parseInt(_bigImageNo) > 0) {
-
+          console.log('found big image');
           _bigImageNo 	= Number(_bigImageNo) - 1;
           _container.find('a:eq('+_bigImageNo+')').css({ width: _firstElemWidth*2 + 'px'});
 
           setTimeout( function() {
+            console.log('time is up. running isotope...');
             _container.isotope({
               masonry: {
                 columnWidth: _firstElemWidth
@@ -47,7 +48,77 @@
       });
     }
   }
+  function _lightbox() {
+    var _el = jQuery(".lightbox");
+
+    if(_el.length > 0) {
+      if(typeof(jQuery.magnificPopup) == "undefined") {
+        console.log('magnificPopup undefined');
+        return false;
+      }
+
+      jQuery.extend(true, jQuery.magnificPopup.defaults, {
+        tClose: 		'Close',
+        tLoading: 		'Loading...',
+
+        gallery: {
+          tPrev: 		'Previous',
+          tNext: 		'Next',
+          tCounter: 	'%curr% / %total%'
+        },
+
+        image: 	{
+          tError: 	'Image not loaded!'
+        },
+
+        ajax: 	{
+          tError: 	'Content not loaded!'
+        }
+      });
+
+      _el.each(function() {
+
+        var _t 			= jQuery(this),
+          options 	= _t.attr('data-plugin-options'),
+          config		= {},
+          defaults 	= {
+            type: 				'image',
+            fixedContentPos: 	false,
+            fixedBgPos: 		false,
+            mainClass: 			'mfp-no-margins mfp-with-zoom',
+            closeOnContentClick: true,
+            closeOnBgClick: 	true,
+            image: {
+              verticalFit: 	true
+            },
+
+            zoom: {
+              enabled: 		false,
+              duration: 		300
+            },
+
+            gallery: {
+              enabled: false,
+              navigateByImgClick: true,
+              preload: 			[0,1],
+              arrowMarkup: 		'<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+              tPrev: 				'Previous',
+              tNext: 				'Next',
+              tCounter: 			'<span class="mfp-counter">%curr% / %total%</span>'
+            }
+          };
+
+        if(_t.data("plugin-options")) {
+          config = jQuery.extend({}, defaults, options, _t.data("plugin-options"));
+        }
+
+        jQuery(this).magnificPopup(config);
+
+      });
+    }
+  }
   $(document).ready(function() {
     _masonryGallery();
+    _lightbox();
   });
 })(jQuery);
