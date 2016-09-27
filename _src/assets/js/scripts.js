2926e5056fae0e016c5906f3fe80cd18f598d66f
azizj1 <aazizmj1@gmail.com>
2016-09-27 06:25:45 -0500

    speaker request form initial

--- _src/assets/js/scripts.js
+++ _src/assets/js/scripts.js
@@ -2316,10 +2316,10 @@
 			LOAD PLUGIN ONLY!
 		 ************************ **/
 		if(jQuery('form.validate-plugin').length > 0) {
-
-			loadScript(/*plugin_path*/ "" + 'form.validate/jquery.form.min.js', function() {
-				loadScript(/*plugin_path*/ "" + 'form.validate/jquery.validation.min.js');
-			});
+			console.log("add validation");
+			// loadScript(/*plugin_path*/ "" + 'form.validate/jquery.form.min.js', function() {
+			// 	loadScript(/*plugin_path*/ "" + 'form.validate/jquery.validation.min.js');
+			// });
 
 		}
 
@@ -2328,76 +2328,76 @@
 		/** Form Validate
 		 ************************ **/
 		if(jQuery('form.validate').length > 0) {
-
-			loadScript(/*plugin_path*/ "" + 'form.validate/jquery.form.min.js', function() {
-				loadScript(/*plugin_path*/ "" + 'form.validate/jquery.validation.min.js', function() {
-
-					if(jQuery().validate) {
-
-						jQuery('form.validate').each(function() {
-
-							var _t 			= jQuery(this),
-								_Smessage 	= _t.attr('data-success') 			|| "Successfully! Thank you!",
-								_Cmessage 	= _t.attr('data-captcha') 			|| "Invalid Captcha!",
-								_Tposition 	= _t.attr('data-toastr-position') 	|| "top-right",
-								_Ttype	 	= _t.attr('data-toastr-type') 		|| "success";
-								_Turl	 	= _t.attr('data-toastr-url') 		|| false;
-
-							// Append 'is_ajax' hidden input field!
-							_t.append('<input type="hidden" name="is_ajax" value="true" />');
-
-							_t.validate({
-								submitHandler: function(form) {
-
-									// Show spin icon
-									jQuery(form).find('.input-group-addon').find('.fa-envelope').removeClass('fa-envelope').addClass('fa-refresh fa-spin');
-
-									jQuery(form).ajaxSubmit({
-
-										target: 	jQuery(form).find('.validate-result').length > 0 ? jQuery(form).find('.validate-result') : '',
-
-										error: 		function(data) { 
-											_toastr("Sent Failed!",_Tposition,"error",false);
-										},
-
-										success: 	function(data) {
-											var data = data.trim();
-
-											// SMTP ERROR
-											if(data == '_failed_') {
-												_toastr("SMTP ERROR! Please, check your config file!",_Tposition,"error",false);
-											}
-
-											// CAPTCHA ERROR
-											else if(data == '_captcha_') {
-												_toastr("Invalid Captcha!",_Tposition,"error",false);
-
-
-											// SUCCESS
-											} else {
-
-												// Remove spin icon
-												jQuery(form).find('.input-group-addon').find('.fa-refresh').removeClass('fa-refresh fa-spin').addClass('fa-envelope');
-
-												// Clear the form
-												jQuery(form).find('input.form-control').val('');
-
-												// Toastr Message
-												_toastr(_Smessage,_Tposition,_Ttype,_Turl);
-											
-											}
-										}
-									});
-
-								}
-							});
-
-						});
-
-					}
-
-				});
-			});
+			console.log("add validation");
+			// loadScript(/*plugin_path*/ "" + 'form.validate/jquery.form.min.js', function() {
+			// 	loadScript(/*plugin_path*/ "" + 'form.validate/jquery.validation.min.js', function() {
+			//
+			// 		if(jQuery().validate) {
+			//
+			// 			jQuery('form.validate').each(function() {
+			//
+			// 				var _t 			= jQuery(this),
+			// 					_Smessage 	= _t.attr('data-success') 			|| "Successfully! Thank you!",
+			// 					_Cmessage 	= _t.attr('data-captcha') 			|| "Invalid Captcha!",
+			// 					_Tposition 	= _t.attr('data-toastr-position') 	|| "top-right",
+			// 					_Ttype	 	= _t.attr('data-toastr-type') 		|| "success";
+			// 					_Turl	 	= _t.attr('data-toastr-url') 		|| false;
+			//
+			// 				// Append 'is_ajax' hidden input field!
+			// 				_t.append('<input type="hidden" name="is_ajax" value="true" />');
+			//
+			// 				_t.validate({
+			// 					submitHandler: function(form) {
+			//
+			// 						// Show spin icon
+			// 						jQuery(form).find('.input-group-addon').find('.fa-envelope').removeClass('fa-envelope').addClass('fa-refresh fa-spin');
+			//
+			// 						jQuery(form).ajaxSubmit({
+			//
+			// 							target: 	jQuery(form).find('.validate-result').length > 0 ? jQuery(form).find('.validate-result') : '',
+			//
+			// 							error: 		function(data) {
+			// 								_toastr("Sent Failed!",_Tposition,"error",false);
+			// 							},
+			//
+			// 							success: 	function(data) {
+			// 								var data = data.trim();
+			//
+			// 								// SMTP ERROR
+			// 								if(data == '_failed_') {
+			// 									_toastr("SMTP ERROR! Please, check your config file!",_Tposition,"error",false);
+			// 								}
+			//
+			// 								// CAPTCHA ERROR
+			// 								else if(data == '_captcha_') {
+			// 									_toastr("Invalid Captcha!",_Tposition,"error",false);
+			//
+			//
+			// 								// SUCCESS
+			// 								} else {
+			//
+			// 									// Remove spin icon
+			// 									jQuery(form).find('.input-group-addon').find('.fa-refresh').removeClass('fa-refresh fa-spin').addClass('fa-envelope');
+			//
+			// 									// Clear the form
+			// 									jQuery(form).find('input.form-control').val('');
+			//
+			// 									// Toastr Message
+			// 									_toastr(_Smessage,_Tposition,_Ttype,_Turl);
+			//
+			// 								}
+			// 							}
+			// 						});
+			//
+			// 					}
+			// 				});
+			//
+			// 			});
+			//
+			// 		}
+			//
+			// 	});
+			// });
 
 		}
 
